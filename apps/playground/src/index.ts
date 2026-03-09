8e68df7366c166aa8a43b53eb29aa7151e49d1e0
Clay Mackenthun <claymackenthun@Clays-Mac-mini.local>
2026-03-09 09:29:20 -0500

    test

--- apps/playground/src/index.ts
+++ apps/playground/src/index.ts
@@ -7,6 +7,10 @@ import path from 'node:path';
     const { DrawMode, CanvasSection, EffectType } = controls;
     const pathToFont = path.join(__dirname, "fonts", "5x7.bdf");
     console.log("Path to font:", pathToFont);
+    const response = await fetch("https://tenor.com/vW5Sam3G2kp.gif");
+    const arrayBuffer = await response.arrayBuffer();
+    // Convert ArrayBuffer to Node.js Buffer
+    const buffer = Buffer.from(arrayBuffer);
     matrix.getCanvas().addCanvasSection(new CanvasSection("mycanvassection", 0, 0, 1, 64, 64, [], true));
     const interval = setInterval(() => matrix.paint(), 5);
     matrix.getCanvas().getCanvasSection("mycanvassection")?.setRepresentation([
@@ -28,14 +32,14 @@ import path from 'node:path';
             id: "demo",
             drawMode: DrawMode.TEXT,
             color: 0x27D3F5,
-            drawModeOptions: { font: "5x7", fontPath: pathToFont, effects: [{ effectType: EffectType.PULSE, effectOptions: { rate: 500 } }, { effectType: EffectType.SCROLLLEFT, effectOptions: { rate: 50 }}] },
+            drawModeOptions: { font: "5x7", fontPath: pathToFont, effects: [{ effectType: EffectType.PULSE, effectOptions: { rate: 500 } }, { effectType: EffectType.SCROLLLEFT, effectOptions: { rate: 50 } }] },
             points: { x: 0, y: 25, z: 0 },
             text: "Welcome to my demo!",
             layer: 1
         }
     ]);
 
-    await new Promise((resolve) => setTimeout(resolve, 1000 * 3));
+    await new Promise((resolve) => setTimeout(resolve, 1000 * 5));
 
     matrix.getCanvas().getCanvasSection("mycanvassection")?.setRepresentation([
         {
@@ -50,7 +54,20 @@ import path from 'node:path';
         }
     ]);
 
-    await new Promise((resolve) => setTimeout(resolve, 1000 * 15));
+    await new Promise((resolve) => setTimeout(resolve, 1000 * 5));
+
+    matrix.getCanvas().getCanvasSection("mycanvassection")?.setRepresentation([
+        {
+            id: "buffer",
+            drawMode: DrawMode.BUFFER,
+            color: 0x000000,
+            buffer: buffer,
+            points: { x: 0, y: 0, z: 0 },
+            width: 32,
+            height: 32,
+            layer: 7
+        }
+    ]);
 
     clearInterval(interval);
     matrix.clear();
