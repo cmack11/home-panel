e9fdf9f6d03030a21dcd1fe94a6b03b659c68763
Clay Mackenthun <claymackenthun@Clays-Mac-mini.local>
2026-03-13 15:54:53 -0500

    test

--- packages/canvas/src/canvas.ts
+++ packages/canvas/src/canvas.ts
@@ -47,6 +47,7 @@ export class Canvas {
         for (const row of pixelsForPainting) {
             for (const pixel of row) {
                 if (pixel) {
+                    console.log(`Painting pixel at (${pixel.getX()}, ${pixel.getY()}) with color ${pixel.getColor().getRGB()} and brightness ${pixel.getBrightness()}`);
                     this.matrix?.fgColor(pixel.getColor().getRGB())?.brightness(pixel.getBrightness())?.setPixel(pixel.getX(), pixel.getY());
                 }
             }
