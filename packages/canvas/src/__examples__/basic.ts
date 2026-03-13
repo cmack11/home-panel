ff82b046a5a7153a45f49542e7e5c709d423dfd8
Clay Mackenthun <claymackenthun@Clays-Mac-mini.local>
2026-03-13 17:15:45 -0500

    test

--- packages/canvas/src/__examples__/basic.ts
+++ packages/canvas/src/__examples__/basic.ts
@@ -2,12 +2,21 @@ import { getMatrix } from "../get-matrix";
 
 
 const matrix = getMatrix();
-for (let x = 0; x < matrix.width(); x++) {
-    const color = { r: Math.random() * 255, g: Math.random() * 255, b: Math.random() * 255 };
-    for (let y = 0; y < matrix.height(); y++) {
-        matrix.fgColor(color).setPixel(x, y);
+const fill = () => {
+    for (let x = 0; x < matrix.width(); x++) {
+        const color = { r: Math.random() * 255, g: Math.random() * 255, b: Math.random() * 255 };
+        for (let y = 0; y < matrix.height(); y++) {
+            matrix.fgColor(color).setPixel(x, y);
+        }
     }
 }
+matrix.afterSync(() => {
+    setTimeout(() => {
+        fill();
+        matrix.sync();
+    }, 1000);
+});
+fill();
 matrix.sync();
 
 await new Promise((resolve) => setTimeout(() => {
