f942f7856aec0bf788ea70ad340d3f6066bfc4c8
Clay Mackenthun <claymackenthun@Clays-Mac-mini.local>
2026-03-14 11:35:50 -0500

    test

--- packages/canvas/src/get-matrix.ts
+++ packages/canvas/src/get-matrix.ts
@@ -11,6 +11,7 @@ export const getMatrix = () => {
 			type: PixelMapperType.U,
 		}),
         showRefreshRate: true,
+        multiplexing: 0,
 	}, {
 		...LedMatrix.defaultRuntimeOptions(),
 		gpioSlowdown: 1,
